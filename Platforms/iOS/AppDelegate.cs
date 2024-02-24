af335a585506ee3e6772db4c6e68a484751be4c8
Charles Belanger <charlesbelanger00@gmail.com>
2024-02-24 03:51:31 -0700

    testing

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LinearSaturn
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
